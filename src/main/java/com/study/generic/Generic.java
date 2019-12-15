c2b0c6384ab14d83b106e5dffa4c05a79d621873
dxq <dxq.qf@qq.com>
2019-12-15 21:10:55 +0800

    p5课程上传包含反射和泛型的练习

---
+++ src/main/java/com/study/generic/Generic.java
@@ -0,0 +1,70 @@
+package com.study.generic;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generic {
+
+    public static void main(String[] args) throws Exception {
+        // demo1();
+        // demo2();
+        demo3();
+    }
+
+
+    // 泛型类型在逻辑上可以看作多个类型，实际上是一个类型
+    public static void demo1() {
+        List a = new ArrayList<String>();
+        List b = new ArrayList<Integer>();
+
+        System.out.println(a.getClass());
+        System.out.println(b.getClass());
+    }
+
+
+    // 泛型擦除 泛型只在编译期间有效，运行期间可通过反射改变类型
+    public static void demo2() throws Exception {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("一个string");
+        // 实际长度为一个，且不能add其它类型的对象 a.add(88); 是编译不通过的。
+        System.out.println("操作前的长度：" + a.size());
+
+        // 反射改变a的形参类型
+        Class clazz = a.getClass();
+        Method method = clazz.getDeclaredMethod("add", Object.class);
+        method.invoke(a, 12);
+        System.out.println("操作后的长度：" + a.size());
+    }
+
+
+    // 泛型通配符
+    public static void demo3() {
+        // 无边界都行
+        ArrayList<Object> list1 = new ArrayList<>();
+        demo3_1(list1);
+        ArrayList<String> list2 = new ArrayList<>();
+        demo3_1(list2);
+
+
+        // demo3_2(list1); 上边界报错
+        demo3_3(list1); // 下边界报错
+
+    }
+
+    // 无边界
+    public static void demo3_1(ArrayList<?> list) {
+        return;
+    }
+
+
+    // 上边界
+    public static void demo3_2(ArrayList<? extends Number> list) {
+        return;
+    }
+
+    // 下边界
+    public static void demo3_3(ArrayList<? super Number> list) {
+        return;
+    }
+}
---
+++ src/main/java/com/study/reflex/ReflexDemo.java
@@ -0,0 +1,35 @@
+package com.study.reflex;
+
+import java.lang.reflect.Method;
+
+public class ReflexDemo {
+
+    public static void main(String[] args) throws Exception {
+        Class clazz = ReflexDemo.class;
+        ReflexDemo demo = (ReflexDemo) clazz.newInstance();
+        System.out.println(clazz.getName());
+        System.out.println(clazz.getSuperclass());
+        System.out.println(clazz.getPackage());
+        System.out.println(clazz.getClassLoader());
+        methodLog();
+    }
+
+    public static void methodLog() throws Exception{
+        Class clazz = ReflexDemo.class;
+        ReflexDemo reflexDemo = (ReflexDemo) clazz.newInstance();
+        Method[] methods = clazz.getMethods();
+        System.out.println(methods.length);
+        /*for (int i = 0;i<methods.length;i++){
+            System.out.println(methods[i]);
+        }*/
+        Method method = clazz.getMethod("methodLog");
+        System.out.println(method.getName());
+
+        methods = clazz.getDeclaredMethods();
+        System.out.println(methods.length);
+
+        method.setAccessible(true);         // 强制调用
+        //method.invoke(reflexDemo);
+    }
+
+}
